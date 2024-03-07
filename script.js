b9b56acce139d9c199bb1bb80428f89a9417c5a2
patrickletran <patrickjmvtran@gmail.com>
2024-03-06 21:30:51 -0800

    1st

---
+++ script.js
@@ -0,0 +1,7 @@
+function showPrompt() {
+    document.getElementById("overlay").style.display = "block";
+}
+
+function closePrompt() {
+    document.getElementById("overlay").style.display = "none";
+}
