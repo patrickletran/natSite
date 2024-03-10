18d9e8e67652483cdffcb3912084cc5489926070
patrickletran <patrickjmvtran@gmail.com>
2024-03-09 19:18:08 -0800

    3/9 1918

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
 function showPrompt() {
-    document.getElementById("overlay").style.display = "block";
+    fetch('messages/3-8-24.txt') // Replace 'message.txt' with the path to your text file
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("message").innerText = data;
+            document.getElementById("overlay").style.display = "block";
+        })
+        .catch(error => console.error('Error fetching message:', error));
 }
 
 function closePrompt() {
     document.getElementById("overlay").style.display = "none";
+}
+
+function loadMessage(date) {
+    fetch('messages/' + date + '.txt') // Adjust the path as needed
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.text();
+        })
+        .then(data => {
+            // Update the message box with the content of the text file
+            document.getElementById("message").innerText = data;
+            // Show the message box
+            document.getElementById("overlay").style.display = "block";
+        })
+        .catch(error => console.error('Error fetching message:', error));
 }
